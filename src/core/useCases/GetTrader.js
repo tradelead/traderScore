8f05d8a1726674055c740e401bff35dff4e666b8
calebmcelroy <caleb@tangentmedia.com>
2019-03-14 16:18:32 -0400

    GetTrader usecase.

---
+++ src/core/useCases/GetTrader.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const traderIDSchema = Joi.string().required().label('Trader ID');
+
+module.exports = class IngressDeposit {
+  constructor({ traderRepo }) {
+    this.traderRepo = traderRepo;
+  }
+
+  async execute(traderID) {
+    const { error } = traderIDSchema.validate(traderID);
+
+    if (error != null) {
+      const humanErr = error.details.map(detail => detail.message).join(', ');
+      const err = new Error(humanErr);
+      err.name = 'BadRequest';
+      throw err;
+    }
+
+    return this.traderRepo.getTrader(traderID);
+  }
+};
