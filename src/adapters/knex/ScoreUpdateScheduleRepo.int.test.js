47fe50d5414861a23b84c94fd8213092721a9d69
calebmcelroy <calebmcelroy@gmail.com>
2019-04-09 09:25:06 -0400

    ScoreUpdateScheduleRepo: implement

--- src/adapters/knex/ScoreUpdateScheduleRepo.int.test.js
+++ src/adapters/knex/ScoreUpdateScheduleRepo.int.test.js
@@ -25,32 +25,44 @@ afterEach(() => {
   Date.now = now;
 });
 
-describe('schedule', () => {
-  let req;
+let req;
 
-  beforeEach(() => {
-    req = {
-      traderID: 'trader1',
-      period: 'day',
-      time: 10000,
-    };
-  });
+beforeEach(() => {
+  req = {
+    traderID: 'trader1',
+    period: 'day',
+    time: 10000,
+  };
+});
 
-  it('works', async () => {
-    await repo.schedule(req);
+it('works', async () => {
+  await repo.schedule(req);
 
-    Date.now = jest.fn().mockReturnValue(1000);
-    let updatesDue = await repo.fetchDue(req);
+  Date.now = jest.fn().mockReturnValue(1000);
+  let updatesDue = await repo.fetchDue(req);
 
-    expect(updatesDue).toHaveLength(0);
+  expect(updatesDue).toHaveLength(0);
 
-    Date.now = jest.fn().mockReturnValue(10000);
-    updatesDue = await repo.fetchDue(req);
+  Date.now = jest.fn().mockReturnValue(10000);
+  updatesDue = await repo.fetchDue(req);
 
-    expect(updatesDue).toHaveLength(1);
-    expect(updatesDue[0]).toMatchObject(req);
-    expect(updatesDue[0].ID).toBeGreaterThan(0);
+  expect(updatesDue).toHaveLength(1);
+  expect(updatesDue[0]).toMatchObject(req);
+  expect(updatesDue[0].ID).toBeGreaterThan(0);
 
-    await repo.markComplete(updatesDue);
-  });
+  await repo.complete(updatesDue);
+
+  updatesDue = await repo.fetchDue(req);
+  expect(updatesDue).toHaveLength(0);
+});
+
+test('complete only deletes items specified', async () => {
+  const [ID1] = await repo.schedule(req);
+  const [ID2] = await repo.schedule(req);
+
+  await repo.complete([{ ID: ID1 }]);
+
+  const updatesDue = await repo.fetchDue(req);
+  expect(updatesDue[0].ID).toEqual(ID2);
+  expect(updatesDue).toHaveLength(1);
 });
