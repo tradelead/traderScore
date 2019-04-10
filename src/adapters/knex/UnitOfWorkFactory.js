22aa1bc6edc27786cd78cafbbf0b3b9bea44381b
calebmcelroy <calebmcelroy@gmail.com>
2019-04-10 11:33:15 -0400

    UnitOfWorkFactory: update to use KnexTrxFactory

--- src/adapters/knex/UnitOfWorkFactory.js
+++ src/adapters/knex/UnitOfWorkFactory.js
@@ -1,5 +1,6 @@
 const BufferedEventEmitter = require('../../utilities/BufferedEventEmitter');
 const KnexUnitOfWork = require('./UnitOfWork');
+const KnexTrxFactory = require('./KnexTrxFactory');
 
 module.exports = class KnexUnitOfWorkFactory {
   constructor(knex, serviceFactories, eventEmitter) {
@@ -8,35 +9,32 @@ module.exports = class KnexUnitOfWorkFactory {
     this.eventEmitter = eventEmitter;
   }
 
-  create() {
-    return new Promise((resolve, reject) => {
-      this.knex.transaction((trx) => {
-        const uow = new KnexUnitOfWork(trx);
-        const uowEvents = new BufferedEventEmitter(this.eventEmitter);
+  async create() {
+    const trx = await new KnexTrxFactory({ knex: this.knex }).create();
+    const uow = new KnexUnitOfWork(trx);
+    const uowEvents = new BufferedEventEmitter(this.eventEmitter);
 
-        Promise.all(Object.keys(this.serviceFactories).map(async (key) => {
-          uow[key] = await this.serviceFactories[key].create({
-            knexConn: trx,
-            events: uowEvents,
-            unitOfWork: uow,
-          });
-        }))
-          .then(() => resolve(uow))
-          .catch(e => reject(e));
-
-        // Only one event will ever be called, therefore to prevent
-        // a memory leak listeners call removeListener for the other.
-        let uowRollbackListener;
-        const uowCompleteListener = () => {
-          uowEvents.removeListener('rollback', uowRollbackListener);
-          uowEvents.flush();
-        };
-        uowRollbackListener = () => {
-          uowEvents.removeListener('complete', uowCompleteListener);
-        };
-        uow.once('complete', uowCompleteListener);
-        uow.once('rollback', uowRollbackListener);
+    await Promise.all(Object.keys(this.serviceFactories).map(async (key) => {
+      uow[key] = await this.serviceFactories[key].create({
+        knexConn: trx,
+        events: uowEvents,
+        unitOfWork: uow,
       });
-    });
+    }));
+
+    // Only one event will ever be called, therefore to prevent
+    // a memory leak listeners call removeListener for the other.
+    let uowRollbackListener;
+    const uowCompleteListener = () => {
+      uowEvents.removeListener('rollback', uowRollbackListener);
+      uowEvents.flush();
+    };
+    uowRollbackListener = () => {
+      uowEvents.removeListener('complete', uowCompleteListener);
+    };
+    uow.once('complete', uowCompleteListener);
+    uow.once('rollback', uowRollbackListener);
+
+    return uow;
   }
 };
