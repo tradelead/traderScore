b089b101f1d8881e720017bcb5bca239289357da
calebmcelroy <calebmcelroy@gmail.com>
2019-06-19 22:00:23 -0400

    BinanceAPI in progress

---
+++ __mocks__/axios.js
@@ -0,0 +1,6 @@
+const sinon = require('sinon');
+
+module.exports = {
+  get: sinon.stub(),
+  post: sinon.stub(),
+};
---
+++ src/adapters/exchangeAPIs/BinanceAPI.unit.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+
+jest.mock('axios');
+
+const axios = require('axios');
+
+describe('getFilledOrders', () => {
+ it('', () => {
+
+ });
+});
+
+describe('getDeposits', () => {
+
+});
+
+describe('getWithdrawals', () => {
+
+});
+
+describe('isRootAsset', () => {
+
+});
+
+describe('getPrice', () => {
+
+});
+
+describe('getMarkets', () => {
+
+});
